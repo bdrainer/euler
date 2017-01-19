c3db0e46a771b4e604b7b00ab7de42e1c6f98672
Brett Drainer <brett@gearlaunch.com>
2017-01-19 11:04:13 -0800

    cleanup

--- src/main/groovy/com/fairwaytech/euler/prob8/LargestProductInSeries.groovy
+++ src/main/groovy/com/fairwaytech/euler/prob8/LargestProductInSeries.groovy
@@ -3,11 +3,7 @@ package com.fairwaytech.euler.prob8
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 /**
- * Problem 7 - 10001st Prime
- *
- * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
- *
- * What is the 10001st prime number?
+ * Problem 8
  */
 @RestController
 class LargestProductInSeries {
